b8d19f43e229ce90cce450773529aa643b0aa225
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-10-08 21:43:50 -0700

    First commit, passing a test or two

---
+++ test/test.js
@@ -0,0 +1,25 @@
+var vows = require('vows'),
+assert = require('assert'),
+SO = require('../lib/strictObject.js');
+
+vows.describe('SO').addBatch({
+	'A simple SO with one property': {
+		topic: function() {
+			return new SO(['name'])
+		},
+		
+		'should allow creation of new objects': function(strictObject){
+			var o = new strictObject();
+		},
+		
+		'should create an object': {
+			topic: function(strictObject){
+				return new strictObject();
+			},
+			
+			'that has a _toObject() function' : function(o){
+				assert.ok(typeof o._toObject === 'function');
+			}
+		}
+	}
+}).export(module);
