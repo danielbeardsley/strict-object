0e9185ac71685fa52098522c344529bac50ecc19
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-10-09 19:15:57 -0700

    Refactor code to work well in a browser
    and added some tests

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ SO = require('../lib/strictObject.js');
 vows.describe('SO').addBatch({
 	'A simple SO with one property': {
 		topic: function() {
-			return new SO(['name'])
+			return SO.create(['name'])
 		},
 
 		'should allow creation of new objects': function(strictObject){
@@ -52,6 +52,57 @@ vows.describe('SO').addBatch({
 
 				o.name(o.undefined_property_0001);
 				assert.equal(o.undefined_property_0002, o.name());
+			}
+		}
+	},
+
+	'An SO with several properties': {
+		topic: function() {
+			return SO.create(['name','age','occupation','height']);
+		},
+
+		'should allow creation of new objects': function(strictObject){
+			var o = new strictObject();
+		},
+
+		'creates objects with a _toObject() function which returns only the attributes that have been set' : function(strictObject){
+			var o = new strictObject();
+			assert.equal(0, Object.keys(o._toObject()));
+			o.name('Danny');
+
+			var keys = Object.keys(o._toObject());
+			assert.equal(keys.length, 1);
+			assert.equal('name', keys[0]);
+		},
+
+		'should create an object that': {
+			topic: function(strictObject){
+				return new strictObject();
+			},
+
+			'has setter functions for each property' : function(o){
+				o.name('Danny')
+				 .age(65)
+				 .occupation('Smuggler')
+				 .height(5.42);
+				var Obj = o._toObject();
+				assert.equal(Obj.name, 'Danny');
+				assert.equal(Obj.age, 65);
+				assert.equal(Obj.occupation, 'Smuggler');
+				assert.equal(Obj.height, 5.42);
+				
+				assert.equal('Danny', o.name());
+			},
+
+			'has getter functions that return the property values' : function(o){
+				o.name('d')
+					.age(1)
+					.occupation('s')
+					.height(true);
+				assert.equal(o.name(), 'd');
+				assert.equal(o.age(), 1);
+				assert.equal(o.occupation(), 's');
+				assert.equal(o.height(), true);
 			}
 		}
 	}
