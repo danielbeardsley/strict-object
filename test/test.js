4e6670527325d157f5a436377dc8a422bc186378
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-10-09 17:58:03 -0700

    More Tests

--- test/test.js
+++ test/test.js
@@ -7,32 +7,51 @@ vows.describe('SO').addBatch({
 		topic: function() {
 			return new SO(['name'])
 		},
-		
+
 		'should allow creation of new objects': function(strictObject){
 			var o = new strictObject();
 		},
-		
+
+		'creates objects with a _toObject() function which returns only the attributes that have been set' : function(strictObject){
+			var o = new strictObject();
+			assert.equal(0, Object.keys(o._toObject()));
+			o.name('Danny');
+
+			var keys = Object.keys(o._toObject());
+			assert.equal(keys.length, 1);
+			assert.equal('name', keys[0]);
+		},
+
 		'should create an object that': {
 			topic: function(strictObject){
 				return new strictObject();
 			},
-			
-			'that has a _toObject() function' : function(o){
-				assert.ok(typeof o._toObject === 'function');
+
+			'has a _toObject() function' : function(o){
+				assert.equal('function', typeof o._toObject);
 			},
-			
+
 			'has a name(val) function that sets the name property' : function(o){
 				o.name('Danny');
 				assert.equal('Danny', o._toObject().name);
+				assert.equal('Danny', o.name());
 			},
-			
+
 			'has a name(val) function that is chainable' : function(o){
 				assert.equal(o, o.name('dummy'));
 			},
-			
+
 			'has a name() function that gets the name property' : function(o){
 				o.name('George');
 				assert.equal('George', o.name());
+			},
+
+			'has a name() function that allows setting to null and undefined' : function(o){
+				o.name(null);
+				assert.equal(null, o.name());
+
+				o.name(o.undefined_property_0001);
+				assert.equal(o.undefined_property_0002, o.name());
 			}
 		}
 	}
